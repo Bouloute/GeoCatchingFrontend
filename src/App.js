2fbc54ec9153bbfb728d4bb2b3ab54eefcc13f91
Bouloute <annaick1995@gmail.com>
2021-01-14 13:46:50 -0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import riddle from './png/003-location.png';
+import history from './png/004-castle.png';
+import artists from './png/005-artist.png';
+import others from './png/006-question.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Joliette Catching</h1>
+      <div className="bandeau">Bandeau explicatif</div>
+
+
+
+      <div className="cards">
+
+        <div className="card">
+          <div className="card__image-holder">
+            <img className="card__image" src={riddle} alt="Art Catching" />
+          </div>
+          <div className="card-title">
+            <h2>
+                Art Catching
+            </h2>
+          </div>
+          
+        </div>
+
+        <div className="card">
+          <div className="card__image-holder">
+            <img className="card__image" src={history} alt="Histoire" />
+          </div>
+          <div className="card-title">
+            <h2>
+                Histoire
+            </h2>
+          </div>
+          
+        </div>
+
+        <div className="card">
+          <div className="card__image-holder">
+            <img className="card__image" src={artists} alt="Artistes" />
+          </div>
+          <div className="card-title">
+            <h2>
+                Artistes
+            </h2>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="card__image-holder">
+            <img className="card__image" src={others} alt="Autres" />
+          </div>
+          <div className="card-title">
+            <h2>
+                Autres
+            </h2>
+          </div>
+        </div>
+
+      </div>
+
+
+
+      <div>Icons made by <a href="https://www.flaticon.com/authors/surang" title="surang">surang</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/nikita-golubev" title="Nikita Golubev">Nikita Golubev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
     </div>
   );
 }
